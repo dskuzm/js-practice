47a812e59439d5d34205b06304c809bdb1b4098d
dskuzm <dskuzm@gmail.com>
2018-04-16 20:58:06 +0300

    added JSON.parse method

--- js/script.js
+++ js/script.js
@@ -1,26 +1,25 @@
-httpGet()
+getApi()
 
-function httpGet() {
-    var xmlHttp = new XMLHttpRequest();
-
+function getApi() {
+    // debugger
+    var inputValue = document.getElementById('search').value,
+        xmlHttp = new XMLHttpRequest();
+    
     xmlHttp.open("GET", "https://api.github.com/search/repositories?q=$test", true);
 
     xmlHttp.onreadystatechange = function (oEvent) {
         if (xmlHttp.readyState === 4) {
             if (xmlHttp.status === 200) {
-              console.log(xmlHttp.responseText)
+                var obj = JSON.parse(xmlHttp.responseText);
+                console.log(obj);
             } else {
-               console.log("Error", xmlHttp.statusText);
+                console.log('Error', xmlHttp.statusText);
             }
         }
     };
 
     xmlHttp.send(null);
 }
-
-
-
-
 
 
 
