9b3a0c40ba3d92364bcd2c5d7e5b14986d967b26
dskuzm <dskuzm@gmail.com>
2018-04-17 13:51:57 +0300

    added search in git

--- js/script.js
+++ js/script.js
@@ -1,27 +1,26 @@
-getApi()
+var inputValue = document.getElementById('search').value,
+    inputSubmit = document.getElementById('submit'),
+    repoLink = document.getElementById('repoLink');
 
-function getApi() {
-    // debugger
+inputSubmit.addEventListener("click", function() {
     var inputValue = document.getElementById('search').value,
         xmlHttp = new XMLHttpRequest();
+        
+        xmlHttp.open('GET', 'https://api.github.com/search/repositories?q=$'+ inputValue, true);
     
-    xmlHttp.open("GET", "https://api.github.com/search/repositories?q=$test", true);
-
-    xmlHttp.onreadystatechange = function (oEvent) {
-        if (xmlHttp.readyState === 4) {
-            if (xmlHttp.status === 200) {
-                var obj = JSON.parse(xmlHttp.responseText);
-                console.log(obj);
-            } else {
-                console.log('Error', xmlHttp.statusText);
+        xmlHttp.onreadystatechange = function (oEvent) {
+            if (xmlHttp.readyState === 4) {
+                if (xmlHttp.status === 200) {
+                    var obj = JSON.parse(xmlHttp.responseText);
+                        var link = (obj.items[6].html_url);//test code
+                        repoLink.setAttribute('href', link);
+                        repoLink.innerHTML = link;
+                        console.log(obj);
+                } else {
+                    console.log('Error', xmlHttp.statusText);
+                }
             }
-        }
-    };
+        };
 
-    xmlHttp.send(null);
-}
-
-
-
-
-
+        xmlHttp.send(null);
+});
