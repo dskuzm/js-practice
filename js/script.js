499476e2ad2189950002c2429967a164f1ea0d26
dskuzm <dskuzm@gmail.com>
2018-04-26 00:06:52 +0300

    refactoring

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
-var inputText = document.querySelector('[data-role=inputText]').value,
-    inputSubmit = document.querySelector('[data-role=searchButton]'),
+var inputSubmit = document.querySelector('[data-role=searchButton]'),
     repoLink = document.querySelector('[data-role=repoLink]');
-    console.log(inputText)
     
 inputSubmit.addEventListener("click", function() {
-    var xmlHttp = new XMLHttpRequest();
-        xmlHttp.open('GET', 'https://api.github.com/search/repositories?q=$qwe', true);
+    var xmlHttp = new XMLHttpRequest(),
+        inputText = document.querySelector('[data-role=inputText]').value;
+        xmlHttp.open('GET', 'https://api.github.com/search/repositories?q=$' + inputText, true);
     
-        xmlHttp.onreadystatechange = function (oEvent) {
+        xmlHttp.onload = function (oEvent) {
             if (xmlHttp.readyState === 4) {
                 if (xmlHttp.status === 200) {
-                    var obj = JSON.parse(xmlHttp.responseText);
-                        var link = (obj.items[6].html_url);//test code
+                    var data = JSON.parse(xmlHttp.responseText);
+                        var link = (data.items[6].html_url);
                         repoLink.setAttribute('href', link);
                         repoLink.innerHTML = link;
-                        console.log(obj)
+                        console.log(data)
                 } else {
                     console.log('Error', xmlHttp.statusText);
                 }
